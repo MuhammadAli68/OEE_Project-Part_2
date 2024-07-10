c96e1684d12f28165ccc236406b0a48021f2b73c
Muhammad Ali <mali@modineer.com>
2024-07-10 15:11:56 -0400

    fixed HSG 1 without group shifts csv path

--- CSV_Server/Routes/hsg4.js
+++ CSV_Server/Routes/hsg4.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 router.get("/4",(req,res)=>{
-    res.sendFile(path.resolve("../data/HSG/HSG Nest Run Data/HSG 4 Nest Run Data/output/withoutgroupshifts.csv"));
+    res.sendFile(path.resolve("T:/HSG/HSG Nest Run Data/HSG 4 Nest Run Data/output/withoutgroupshifts.csv"));
 });
 
 router.get("/4/shifts",(req,res)=>{
-    res.sendFile(path.resolve("../data/HSG/HSG Nest Run Data/HSG 4 Nest Run Data/output/withgroupshifts.csv"));
+    res.sendFile(path.resolve("T:/HSG/HSG Nest Run Data/HSG 4 Nest Run Data/output/withgroupshifts.csv"));
 });
 module.exports = router;
