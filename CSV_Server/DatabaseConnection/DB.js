07edeee44b60581472badd042eacede002fdd619
Muhammad Ali <mali@modineer.com>
2024-08-08 12:58:31 -0400

    using environment varaibles instead of hardcoding values in config

--- CSV_Server/DatabaseConnection/DB.js
+++ CSV_Server/DatabaseConnection/DB.js
@@ -1,9 +1,9 @@
 const config = {
-    user: 'OEEadmin', // better stored in an app setting such as process.env.DB_USER
-    password: '!CGd@6ajWiX3Hj5', // better stored in an app setting such as process.env.DB_PASSWORD
-    server: 'myfreeoeedbserver.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
-    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: 'myfreeoeedb', // better stored in an app setting such as process.env.DB_NAME
+    user: process.env.DB_USER, // better stored in an app setting such as process.env.DB_USER
+    password: process.env.DB_PASSWORD, // better stored in an app setting such as process.env.DB_PASSWORD
+    server: process.env.DB_SERVER, // better stored in an app setting such as process.env.DB_SERVER
+    port: Number(process.env.DB_PORT), // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: process.env.DB_NAME, // better stored in an app setting such as process.env.DB_NAME
     authentication: {
         type: 'default'
     },
